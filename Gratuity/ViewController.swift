971e9e20432bb11597b0e11ff9039aa8331eca3e
Keyan Kazemian <kdkazemi@uci.edu>
2020-01-09 17:04:09 -0800

    git init

---
+++ Gratuity/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  Gratuity
+//
+//  Created by Keyan Kazemian on 1/9/20.
+//  Copyright © 2020 Keyan Kazemian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func calcTip(_ sender: Any) {
+        let bill = Double(billField.text!) ?? 0
+        let tips = [0.1, 0.15, 0.2]
+        
+        let tip = bill * tips[tipControl.selectedSegmentIndex]
+        
+        let total = bill + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+
+    }
+}
+
